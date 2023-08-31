0d9bd3cb4f11b2baadc7f4acc886f5a9e574fd32
Omkar Anabathula <aomkarg2003@gmail.com>
2023-08-31 19:30:00 +0530

    dfn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -51,7 +51,7 @@ function Login() {
                   name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="pizzamania@gmail.com"
                   onChange={handleLogin}
                   required=""
                 />
@@ -81,6 +81,7 @@ function Login() {
                       aria-describedby="remember"
                       type="checkbox"
                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                      placeholder="1234567"
                       required=""
                     />
                   </div>
